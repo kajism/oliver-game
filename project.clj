963190f45601aa2a6ddd36bad5b6f5d6ec9e1b4d
Karel Miarka <karel.miarka@seznam.cz>
2020-06-08 22:22:03 +0200

    Version 0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject oliver-game "0.0.1-SNAPSHOT"
+(defproject oliver-game "0.0.1"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.597"]
                  ;;[org.clojure/core.async "1.1.587"]

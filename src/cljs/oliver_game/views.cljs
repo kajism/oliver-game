6bffe35be28de4ca51379f0cbbaf06bda285d545
Karel Miarka <karel.miarka@seznam.cz>
2020-08-25 10:32:41 +0200

    Nova mapa

--- src/cljs/oliver_game/views.cljs
+++ src/cljs/oliver_game/views.cljs
@@ -18,29 +18,50 @@
               {:img "big-mac.png"
                :cena 80}])
 
+(defn button [x y label on-click color]
+  [:g {:transform (str "translate(" x ", " y ")") :on-click on-click}
+   [:rect {:width 200 :height 50 :stroke "black" :fill color}]
+   [:text {:x 10 :y 35 :fill "black" :style {:font-size "200%" :cursor "pointer" :user-select "none"}} label]])
+
+
+(defn inventory []
+  (let [x0 200
+        y0 200]
+    [:g
+     [:rect {:x x0 :y y0 :width 500 :height 500 :stroke "black" :fill "lightgray"}]
+     (doall
+      (for [x (range 5)
+            y (range 5)]
+        [:rect {:x (+ x0 (* 100 x)) :y (+ y0 (* 100 y)) :width 100 :height 100 :stroke "black" :fill "yellow"}]))]))
+
+(defn mini-adventures []
+  (let [show-villager? (reagent/atom false)
+        show-inventory? (reagent/atom false)]
+    (fn []
+      [:svg {:width 1000 :viewBox "0 0 1509 1104"}
+       [:image {:x 0 :y 0 :xlink-href "/img/mapa-hry.png" :width 1509 :height 1104}]
+       (when @show-villager?
+         [:image {:x 400 :y 350 :width 310 :height 467 :xlink-href "/img/villager.png" :on-click #(reset! show-villager? false)}])
+       [:rect {:x 643 :y 430 :width 20 :height 100 :fill "yellow" :fill-opacity "0.04" :stroke "none" :on-click #(reset! show-villager? true)}]
+       [button 50 50 "Inventář" #(swap! show-inventory? not) "lightgray"]
+       (when @show-inventory?
+         [inventory])])))
+
 (defn panel []
   (let [show-milk-shakes? (re-frame/subscribe [::subs/show? :show-milk-shakes?])
         celkem (reagent/atom 0)
         ;;pokladna (reagent/atom (int (rand 84)))
         burger-idx (reagent/atom (int (rand 3)))
         show-game? (reagent/atom false)
+        mini-adventures? (reagent/atom true)
         chat-input (reagent/atom "")]
     (fn []
       [:div
        [:h1 "Oliver's Game Page"]
-       (if-not @show-game?
-         [:div
-          [:h3 "Vážení návštěvníci, rád bych vám sdělil, že moje hra sice není úplně dokončená, ale můžete si vyskoušet tuto hru a postupně ji budeme vylepšovat."]
-          [:h3 "Chat"]
-          [:pre @(re-frame/subscribe [::subs/chat])]
-          [:input {:type "text" :on-change #(reset! chat-input (etv %)) :value @chat-input
-                   :style {:width "600px"}} ]
-          [:button {:on-click #(do
-                                 (re-frame/dispatch [::events/send @chat-input])
-                                 (reset! chat-input ""))} "Odeslat"]
-          [:br]
-          [:br]
-          [:button {:on-click #(reset! show-game? true)} "Hrát hru"]]
+       (cond
+         @mini-adventures?
+         [mini-adventures]
+         @show-game?
          [:svg {:width 1000 :viewBox "0 0 1316 933"}
           [:image {:x 0 :y 0 :xlink-href "/img/background.png" :width 1316 :height 933}]
           [:circle {:cx 990 :cy 170 :r 27 :fill "#f0f0f0" :on-click #(re-frame/dispatch [::events/toggle :show-mayonaise?])
@@ -78,12 +99,11 @@
           #_[:g {:transform "translate(800, 2)" :on-click #()}
              [:rect {:width 110 :height 50 :stroke "black" :fill "lightgray"}]
              [:text {:x 10 :y 35 :fill "black" :style {:font-size "200%" :cursor "pointer" :user-select "none"}} "MENU"]]
-          [:g {:transform "translate(1080, 2)" :on-click #(do
-                                                            (re-frame/dispatch [::events/hide-all])
-                                                            (reset! burger-idx (int (rand 3)))
-                                                            (swap! celkem (partial + -5)))}
-           [:rect {:width 200 :height 50 :stroke "black" :fill "pink"}]
-           [:text {:x 10 :y 35 :fill "black" :style {:font-size "200%" :cursor "pointer" :user-select "none"}} "ODMÍTNOUT"]]
+          [button 1080 2 "ODMÍTNOUT" #(do
+                                        (re-frame/dispatch [::events/hide-all])
+                                        (reset! burger-idx (int (rand 3)))
+                                        (swap! celkem (partial + -5)))
+           "pink"]
           [:g {:transform "translate(10, 12)" :on-click #()}
            [:rect {:width 60 :height 35 :stroke "white" :fill "white" :fill-opacity "0.5"}]
            [:text {:x 0 :y 27 :fill "DarkGreen"
@@ -98,4 +118,20 @@
                    }
             "$" (get-in burgery [@burger-idx :cena])]]
           
-          ])])))
+          ]
+         :default
+         [:div
+          [:h3 "Vážení návštěvníci, rád bych vám sdělil, že moje hra sice není úplně dokončená, ale můžete si vyskoušet tuto hru a postupně ji budeme vylepšovat."]
+          [:h3 "Chat"]
+          [:pre @(re-frame/subscribe [::subs/chat])]
+          [:input {:type "text" :on-change #(reset! chat-input (etv %)) :value @chat-input
+                   :style {:width "600px"}} ]
+          [:button {:on-click #(do
+                                 (re-frame/dispatch [::events/send @chat-input])
+                                 (reset! chat-input ""))} "Odeslat"]
+          [:br]
+          [:br]
+          [:button {:on-click #(reset! show-game? true)} "Hrát hru"][:br][:br]
+          [:button {:on-click #(reset! mini-adventures? true)} "Hrát hru Mini adventures!"]]
+
+         )])))
